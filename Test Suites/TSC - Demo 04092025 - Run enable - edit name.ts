d27c6c6f3ed9e8aede32acdc3d3853ef0c015a99
lieuhuynh1811 <lieu.huynh@katalon.com>
2025-09-29 15:28:15 +0700

    update

--- Test Suites/TSC - Demo 04092025 - Run enable - edit name.ts
+++ Test Suites/TSC - Demo 04092025 - Run enable - edit name.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>TSC - Demo 04092025 - Run enable</name>
+   <name>TSC - Demo 04092025 - Run enable - edit name</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
    <executionMode>PARALLEL</executionMode>
