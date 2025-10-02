d24fd3307fe82961b012053111e40577ae31bc65
lieuhuynh1811 <lieu.huynh@katalon.com>
2025-10-02 13:56:06 +0700

    update

---
+++ Test Suites/New Dynamic Test Suite 1.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>New Dynamic Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0ff7c2ca-9cc7-44eb-ae4b-969aee0d6005</testSuiteGuid>
+   <filteringBuiltIn>com.kms.katalon.execution.platform.DynamicBuiltInSearch</filteringBuiltIn>
+   <filteringExtension></filteringExtension>
+   <filteringPlugin></filteringPlugin>
+   <filteringText>tag=(dynamic) </filteringText>
+</FilteringTestSuiteEntity>
---
+++ Test Suites/New Dynamic Test Suite.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>New Dynamic Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7702e584-acc9-4a31-8f32-c511f23cb560</testSuiteGuid>
+   <filteringBuiltIn>com.kms.katalon.execution.platform.DynamicBuiltInSearch</filteringBuiltIn>
+   <filteringExtension></filteringExtension>
+   <filteringPlugin></filteringPlugin>
+   <filteringText>tag=(dynamic) </filteringText>
+</FilteringTestSuiteEntity>
